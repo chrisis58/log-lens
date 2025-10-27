f03baf7881237f050906da745f4a69f54fd3c572
chrisis <chrisis58@foxmail.com>
2025-10-27 22:26:22 +0800

    feat: define the module structure of log-lens-server

---
+++ server/log-lens-server-app/src/main/java/cn/teacy/loglens/LogLensServerApplication.java
@@ -0,0 +1,12 @@
+package cn.teacy.loglens;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogLensServerApplication {
+
+    public static void main(String[] args) {
+        org.springframework.boot.SpringApplication.run(LogLensServerApplication.class, args);
+    }
+
+}
---
+++ server/log-lens-server-app/src/main/java/cn/teacy/loglens/controller/ingestion/IngestionController.java
@@ -0,0 +1,16 @@
+package cn.teacy.loglens.controller.ingestion;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Collect log data from log-lens-client
+ *
+ */
+@RestController
+@RequestMapping("/ingestion")
+public class IngestionController {
+
+    // TODO: Implement log ingestion endpoints
+
+}
